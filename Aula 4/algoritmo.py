9adf92bfa4eb982a0ef285426912dabcbfe223eb
FugetsuHuiGuoRou <cassandrapascal2010@gmail.com>
2026-03-28 10:36:02 -0300

    feat:novas aulas

---
+++ Aula 4/algoritmo.py
@@ -0,0 +1,10 @@
+
+algoritmo = int(input("Digite um número: "))
+
+if algoritmo  >= 0 and algoritmo <= 9:
+    print("O número está correto")
+else: 
+
+    print("O número está incorreto")
+
+
---
+++ Aula 4/horas_trabalhadas.py
@@ -0,0 +1,24 @@
+superman = str(input("Digite o seu turno de trabalho: "))
+superman = float(input("Digite a quantidade de horas trabalhadas: "))
+
+if superman == "N" or superman == "n":
+    kriptonita = 45.0
+else:
+    kriptonita = 37.5 #KRIPNOTITA É O VALOR DA HORA TRABALHADA
+
+    batman = superman * kriptonita
+    print("O valor do salário é: ", batman)
+    
+
+
+#KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+
+
+
+
+
+
+
+
+
+

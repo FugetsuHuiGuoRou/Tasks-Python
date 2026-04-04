ff270d7134790a46574fda6b86fc3f3c0be4605a
Seu Nome <seuemail@exemplo.com>
2026-04-04 02:31:59 -0300

    teste

---
+++ Aula 0/for_while.py
@@ -0,0 +1,10 @@
+notas = [8.5, 9.0, 7.5, 9.5, 8.0]
+# Usando um loop for para iterar sobre a lista de notas
+for nota in notas:
+    print(f"A nota é: {nota}")
+# Usando um loop while para iterar sobre a lista de notas
+index = 0
+while index < len(notas):
+    print(f"A nota é: {notas[index]}")
+    index += 1
+    
---
+++ Aula 0/lista.py
@@ -0,0 +1,8 @@
+lista_numeros = [1, 2, 3, 4, 5]
+
+
+print(lista_numeros[0]) # Acessando o primeiro elemento da lista
+print(lista_numeros[1]) # Acessando o segundo elemento da lista     
+print(lista_numeros[2]) # Acessando o terceiro elemento da lista
+print(lista_numeros[3]) # Acessando o quarto elemento da lista
+print(lista_numeros[4]) # Acessando o quinto elemento da lista
